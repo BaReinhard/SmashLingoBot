6a9793c51256d8c7506eaad09c6c7451bc591910
brett.reinhard <brett.reinhard@ticketmaster.com>
2020-05-22 10:10:23 -0700

    Updated where secrets get pulled from

---
+++ .secrets.js
@@ -0,0 +1,15 @@
+const CLIENT_ID = process.env.CLIENT_ID;
+const CLIENT_SECRET = process.env.CLIENT_SECRET;
+const USERNAME = process.env.USERNAME;
+const PASSWORD = process.env.PASSWORD;
+const USER_AGENT = process.env.USERAGENT;
+const SUBREDDIT = process.env.SUBREDDIT;
+
+module.exports = {
+    CLIENT_ID,
+    CLIENT_SECRET,
+    USERNAME,
+    PASSWORD,
+    USER_AGENT,
+    SUBREDDIT
+}
